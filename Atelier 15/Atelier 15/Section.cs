f059168987608b8b59d2c33a7c4a7e9abcce11f0
David Choquet <david.choquet44@gmail.com>
2017-02-06 08:49:16 -0500

    Ajouts classe Section

--- Atelier 15/Atelier 15/Section.cs
+++ Atelier 15/Atelier 15/Section.cs
@@ -20,7 +20,24 @@ namespace AtelierXNA
         Vector2 Position { get; set; }
         Vector2 Étendue { get; set; }
         Vector2 Extrémité { get; set; }
-        bool EstVisible { get; set; }
+        double NormeÉtendue { get; set; }
+        bool estVisible;
+        public bool EstVisible
+        {
+            get { return estVisible;}
+            set
+            {
+                bool tampon = estVisible;
+                estVisible = value;
+                if(tampon != estVisible)
+                {
+                    foreach (GameComponent x in Components)
+                    {
+                        x.Enabled = !x.Enabled;
+                    }
+                }
+            }
+        }
         List<GameComponent> Components { get; set; }
         public Section(Game game, Vector2 position, Vector2 étendue)
             : base(game)
@@ -29,21 +46,39 @@ namespace AtelierXNA
             Étendue = étendue;
         }
 
-        /// <summary>
-        /// Allows the game component to perform any initialization it needs to before starting
-        /// to run.  This is where it can query for any required services and load content.
-        /// </summary>
         public override void Initialize()
         {
             Extrémité = Position + Étendue;
-
+            NormeÉtendue = Math.Sqrt(Math.Pow(Étendue.X, 2) + Math.Pow(Étendue.Y, 2));
             base.Initialize();
         }
 
-        /// <summary>
-        /// Allows the game component to update itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public void IsItIn(Vector2 position, Vector2 direction)
+        {
+            if (CheckIfIn(position) || CheckIfIn(position + direction))
+            {
+                EstVisible = true;
+            }
+            else
+            {
+                EstVisible = false;
+            }
+        }
+
+        public bool CheckIfIn(Vector2 position)
+        {
+            bool value = false;
+            if(position.X <= Étendue.X && position.X >= Position.X && position.Y <= Étendue.Y && position.Y >= Position.Y)
+            {
+                value = true;
+            }
+            return value;
+        }
+        public void AddComponent(GameComponent x)
+        {
+            Components.Add(x);
+        }
+
         public override void Update(GameTime gameTime)
         {
             // TODO: Add your update code here
