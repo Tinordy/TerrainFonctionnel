e269f2b3fe5819b3ae3088d3a24af180d0b4a1b3
David Choquet <david.choquet44@gmail.com>
2017-02-20 10:15:43 -0500

    remis les lignes qui etaient en comm

--- Atelier 15/Atelier 15/Atelier.cs
+++ Atelier 15/Atelier 15/Atelier.cs
@@ -31,7 +31,7 @@ namespace AtelierXNA
 
         // server related properties
 
-        string IP = "127.0.0.1";
+        string IP = "172.17.106.122";
         int PORT = 5001;
         int BUFFER_SIZE = 2048;
         byte[] readBuffer;
@@ -82,25 +82,25 @@ namespace AtelierXNA
             Components.Add(CaméraJeu);
             Sections = new List<Section>();
             Components.Add(new Afficheur3D(this));
-            //Components.Add(new ArrièrePlanDéroulant(this, "CielÉtoilé", INTERVALLE_MAJ_STANDARD));
-            //for (int i = 0; i < 2; ++i)
-            //{
-            //    for (int j = 0; j < 2; ++j)
-            //    {
-            //        Section newSection = new Section(this, new Vector2(200 * i, 100 * j), new Vector2(200, 200), 1f, Vector3.Zero, Vector3.Zero, new Vector3(200, 25, 200), new string[] { "Herbe", "Sable" }, INTERVALLE_MAJ_STANDARD);
-            //        Sections.Add(newSection);
-            //        ListeSections.Add(newSection);
-            //    }
-            //}
-            //foreach (Section s in ListeSections)
-            //{
-            //    Components.Add(s);
-            //}
-            
+            Components.Add(new ArrièrePlanDéroulant(this, "CielÉtoilé", INTERVALLE_MAJ_STANDARD));
+            for (int i = 0; i < 2; ++i)
+            {
+                for (int j = 0; j < 2; ++j)
+                {
+                    Section newSection = new Section(this, new Vector2(200 * i, 100 * j), new Vector2(200, 200), 1f, Vector3.Zero, Vector3.Zero, new Vector3(200, 25, 200), new string[] { "Herbe", "Sable" }, INTERVALLE_MAJ_STANDARD);
+                    Sections.Add(newSection);
+                    ListeSections.Add(newSection);
+                }
+            }
+            foreach (Section s in ListeSections)
+            {
+                Components.Add(s);
+            }
+
             //Components.Add(new Terrain(this, 1f, Vector3.Zero, Vector3.Zero, new Vector3(256, 25, 256), "GrandeCarte", "DétailsTerrain", 5, INTERVALLE_MAJ_STANDARD));
             //Components.Add(new Terrain(this, 1f, Vector3.Zero, Vector3.Zero, new Vector3(200, 25, 200), "CarteTest", "DétailsTerrain", 5, INTERVALLE_MAJ_STANDARD));
             Components.Add(new AfficheurFPS(this, "Arial20", Color.Red, INTERVALLE_CALCUL_FPS));
-            //Components.Add(new Piste(this, 1f, Vector3.Zero, Vector3.Zero, INTERVALLE_MAJ_STANDARD, 20000, 20000));
+            Components.Add(new Piste(this, 1f, Vector3.Zero, Vector3.Zero, INTERVALLE_MAJ_STANDARD, 20000, 20000));
 
             //Services.AddService(typeof(Random), new Random());
             Services.AddService(typeof(RessourcesManager<SpriteFont>), new RessourcesManager<SpriteFont>(this, "Fonts"));
