d86deab1e33d00c90279ede427c8462f58776ea2
David Choquet <david.choquet44@gmail.com>
2017-02-22 08:34:34 -0500

    ip dave

--- Atelier 15/Atelier 15/Atelier.cs
+++ Atelier 15/Atelier 15/Atelier.cs
@@ -31,7 +31,7 @@ namespace AtelierXNA
 
         // server related properties
 
-        string IP = "172.17.106.132";
+        string IP = "172.17.106.122";
         int PORT = 5001;
         int BUFFER_SIZE = 2048;
         byte[] readBuffer;
